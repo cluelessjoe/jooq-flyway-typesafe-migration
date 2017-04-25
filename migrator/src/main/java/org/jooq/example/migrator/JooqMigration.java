d38c780f1fa2b2ed470bca436f8ea6653238e927
Joseph Pachod <joseph.pachod@berger-levrault.fr>
2017-04-25 18:22:50 +0200

    use JDBCUtils.dialect in JooqMigration

--- migrator/src/main/java/org/jooq/example/migrator/JooqMigration.java
+++ migrator/src/main/java/org/jooq/example/migrator/JooqMigration.java
@@ -4,6 +4,7 @@ import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
+import org.jooq.tools.jdbc.JDBCUtils;
 
 import java.sql.Connection;
 
@@ -11,8 +12,15 @@ public abstract class JooqMigration implements JdbcMigration {
 
     @Override
     public final void migrate(Connection connection) throws Exception {
-        DSLContext create = DSL.using(connection, SQLDialect.H2);
+        DSLContext create = DSL.using(connection, extraDialectOrThrow(connection));
         migrate(connection, create);
+    }
+
+    private SQLDialect extraDialectOrThrow(Connection connection) {
+        SQLDialect dialect = JDBCUtils.dialect(connection);
+        if(SQLDialect.DEFAULT.equals(dialect))
+            throw new IllegalStateException("Dialect couldn't be deducted from connection " + connection);
+        return dialect;
     }
 
     public abstract void migrate(Connection connection, DSLContext create) throws Exception;
